3a6b0cae4bab4f2bf16f9ad1a4ff5fb3e7db0c76
Aleksei Dischenko <nomad@sovcombank.ru>
2024-11-16 20:20:25 +0300

    Generating diff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { program } from 'commander';
 import { getFile, getDiff } from './main,js';
 
 const run = () => {
-    console.log('Running!');
     program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
@@ -11,14 +10,11 @@ const run = () => {
     program.option('-f, --format [type]', 'output format')
     .argument('<filepath1>').argument('<filepath2>')
     .action((first, second, options) => {
-        console.log('action '+first+' '+second);
         const json1 = getFile(first);
         const json2 = getFile(second);
-        console.log(json1,json2);
         const diff = getDiff(json1,json2);
-        console.log(diff);
+        console.log(`\n{${diff}\n}`);
     });
-    
     program.parse();
 };
 export default run;
