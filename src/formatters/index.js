49cb0f98f812c193eef4f11f190eb1d6ddebcb3d
a-dishenko <the.teknomad@gmail.com>
2025-06-10 12:44:29 +0300

    JSON formatter, test

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,8 +2,14 @@
 import formatPlainDiff from './plain.js';
 import formatStylishDiff from './stylish.js';
 
+const formatJsonDiff = (diff) => {
+  return JSON.stringify(diff, null, 2);
+}
+
 const getFormatter = (format) => {
   switch (format) {
+    case 'json':
+        return formatJsonDiff;
     case 'plain':
       return formatPlainDiff;
     case 'stylish':
